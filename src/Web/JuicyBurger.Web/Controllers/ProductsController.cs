bd48da6dd6e4e6ea078e3f65c3329aef4e91cd1e
Daniel Bankov <bankovdaniel@gmail.com>
2019-08-05 23:15:26 +0300

    Created Products static pages and about page

---
+++ src/Web/JuicyBurger.Web/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JuicyBurger.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult BurgersMenu()
+        {
+            return View();
+        }
+
+        public IActionResult FriesMenu()
+        {
+            return View();
+        }
+
+        public IActionResult DrinksMenu()
+        {
+            return View();
+        }
+    }
+}
