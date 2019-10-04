a5b3c2bbd534df80ce7dc1044ed6e2d9ac37ad6f
Daniel Bankov <bankovdaniel@gmail.com>
2019-10-04 21:11:03 +0300

    Added antiforgery token

--- src/Web/JuicyBurger.Web/Startup.cs
+++ src/Web/JuicyBurger.Web/Startup.cs
@@ -90,7 +90,11 @@ namespace JuicyBurger.Web
             Cloudinary cloudinaryUtility = new Cloudinary(accountCredentials);
             services.AddSingleton(cloudinaryUtility);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
